e505c809da7e18600726a4ee328d89baef4d2ccc
Tjark Wilhelm Hoeck <tjark.wilhelm.hoeck@mni.thm.de>
2016-08-10 13:51:21 +0200

    init: empty project strucutre, assemblies and build paths

---
+++ ARSnovaPPIntegration/source/ARSnovaPPIntegration.Test/BusinessTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ARSnovaPPIntegration.Test
+{
+    [TestClass]
+    public class BusinessTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ ARSnovaPPIntegration/source/ARSnovaPPIntegration.Test/CommunicationTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ARSnovaPPIntegration.Test
+{
+    [TestClass]
+    public class CommunicationTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
