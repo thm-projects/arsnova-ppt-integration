bb9cb4f5b51b68030c690e8fde26b26735ba59ce
Tjark Wilhelm Hoeck <tjark.wilhelm.hoeck@mni.thm.de>
2016-08-11 14:49:34 +0200

    LocalizationService (i18n)

---
+++ ARSnovaPPIntegration/source/ARSnovaPPIntegration.Common.Contract/ILocalizationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ARSnovaPPIntegration.Common.Contract
+{
+    public interface ILocalizationService
+    {
+        /// <summary>
+        /// Translates text based on the current culture.
+        /// </summary>
+        /// <param name="text">The text for translation.</param>
+        /// <returns></returns>
+        string Translate(string text);
+    }
+}
