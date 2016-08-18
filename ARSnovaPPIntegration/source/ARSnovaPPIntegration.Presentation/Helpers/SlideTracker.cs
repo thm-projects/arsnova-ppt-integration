1ded44fc87c8a5a38ab0c83f072efcf0c869c841
Tjark Wilhelm Hoeck <tjark.wilhelm.hoeck@mni.thm.de>
2016-08-18 12:21:32 +0200

    Add Slidetracker

---
+++ ARSnovaPPIntegration/source/ARSnovaPPIntegration.Presentation/Helpers/SlideTracker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.PowerPoint;
+
+namespace ARSnovaPPIntegration.Presentation.Helpers
+{
+    public static class SlideTracker
+    {
+        public static Slide CurrentSlide
+        {
+            get
+            {
+                try
+                {
+                    return Globals.ThisAddIn.Application.ActiveWindow.View.Slide as Slide;
+                }
+                catch (COMException)
+                {
+                    // currently in view || no slide selected
+                    return null;
+                }
+            }
+        }
+    }
+}
