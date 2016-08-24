13ee1eaa6e157cc9ca60f74a8c44b11f6ac37cd2
Tjark Wilhelm Hoeck <tjark.wilhelm.hoeck@mni.thm.de>
2016-08-24 15:21:52 +0200

    Play around with arsnova.eu API - part 2.

--- ARSnovaPPIntegration/source/ARSnovaPPIntegration.Communication/ArsnovaEuService.cs
+++ ARSnovaPPIntegration/source/ARSnovaPPIntegration.Communication/ArsnovaEuService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -16,36 +17,115 @@ namespace ARSnovaPPIntegration.Communication
 {
     public class ArsnovaEuService : IArsnovaEuService
     {
+        private readonly List<Tuple<string, string>> arsnovaEuHeaders = new List<Tuple<string, string>>
+        {
+            new Tuple<string, string>("Origin", "https://arsnova.eu"),
+            new Tuple<string, string>("X-Requested-With", "XMLHttpRequest"),
+            new Tuple<string, string>("Accept-Encoding", "gzip, deflate, br"),
+            new Tuple<string, string>("Accept-Language", "de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4")
+        };
+
+        private CookieContainer authentificatedCookieContainer;
+
+        public ArsnovaEuService()
+        {
+            // Login as guest by default
+            // TODO how long does the cookie remain? check it everytime before a HTTP-Request is fired!
+            this.authentificatedCookieContainer = this.Login();
+        }
+
         public SessionModel CreateNewSession()
         {
             return this.CreateNewSessionTask();
         }
 
+        private CookieContainer Login()
+        {
+            var url = "https://arsnova.eu/api/auth/login?type=guest&user=" + this.GenerateGuestName() + "&_dc=" +
+                      this.ConvertToUnixTimestampString(DateTime.Now);
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            var cookieContainer = new CookieContainer();
+            request.CookieContainer = cookieContainer;
+            // TODO sum the next part up! bad code quality in here!
+            request.Method = "GET";
+            request.Host = "arsnova.eu";
+            request.KeepAlive = true;
+            request.ContentType = "application/json";
+            request.Accept = "*/*";
+            request.Referer = "https://arsnova.eu/mobile/";
+
+            // TODO swap this one, too! (differ from http-method)
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+
+                using (var reader = new StreamReader(response.GetResponseStream()))
+                {
+                    if (response.IsMutuallyAuthenticated)
+                    {
+                        // test purpose only
+                    }
+                }
+            }
+            catch (WebException webException)
+            {
+                throw new ArsnovaCommunicationException("Error while creating new session", webException);
+            }
+
+            return request.CookieContainer;
+
+        }
+
+        private string GenerateGuestName()
+        {
+            const string chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+            const int stringLength = 5;
+            const string randomstring = "Guest";
+            var random = new Random();
+
+            return randomstring + new string(Enumerable.Repeat(chars, stringLength)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
         private SessionModel CreateNewSessionTask()
         {
-            var url = "https://arsnova.thm.de/api/session/";
+            var url = "https://arsnova.eu/api/session/?_dc=" + this.ConvertToUnixTimestampString(DateTime.Now);
 
             var request = (HttpWebRequest) WebRequest.Create(url);
+            request.CookieContainer = this.authentificatedCookieContainer;
+
+            // The Headers with Properties should be setted with them
             request.Method = "POST";
+            request.Host = "arsnova.eu";
+            request.KeepAlive = true;
             request.ContentType = "application/json";
-            request.Accept = "application/json";
+            request.Accept = "*/*";
+            request.Referer = "https://arsnova.eu/mobile/";
+
+            foreach (var arsnovaEuHeader in this.arsnovaEuHeaders)
+            {
+                request.Headers.Set(arsnovaEuHeader.Item1, arsnovaEuHeader.Item2);
+            }
 
             var requestBody = "{" +
-                               "\"name\":\"testQuizOffice\"" +
-                               "\"shortName\":\"tqo\"" +
-                               "\"courseId\":\"null\"" +
-                               "\"courseType\":\"null\"" +
-                               "\"creationTime\":\"" + DateTime.Now.ToString() + "\"" +
-                               "\"ppAuthorName\":\"Tjark Wilhelm Hoeck\"" +
-                               "\"ppAuthorMail\":\"tjark.wilhelm.hoeck@mni.thm.de\"" +
-                               "\"ppLogo\":\"null\"" +
-                               "\"ppSubject\":\"null\"" +
-                               "\"ppLicense\":\"null\"" +
-                               "\"ppDescription\":\"null\"" +
-                               "\"ppFaculty\":\"null\"" +
-                               "\"ppLevel\":\"null\"" +
-                               "\"sessionType\":\"null\"" +
+                               "\"courseId\": \"null\"," +
+                               "\"courseType\": \"null\"" +
+                               "\"creationTime\": \"" + this.ConvertToUnixTimestampString(DateTime.Now) + "\"," +
+                               "\"name\": \"testQuizOffice\"" +
+                               "\"ppAuthorMail\": \"tjark.wilhelm.hoeck@mni.thm.de\"," +
+                               "\"ppAuthorName\": \"Tjark Wilhelm Hoeck\"," +
+                               "\"ppDescription\": \"null\"," +
+                               "\"ppFaculty\": \"null\"," +
+                               "\"ppLevel\": \"null\"," +
+                               "\"ppLicense\": \"null\"," +
+                               "\"ppLogo\": \"null\"," +
+                               "\"ppSubject\": \"null\"," +
+                               "\"ppUniversity\": \"null\"," +
+                               "\"sessionType\": \"null\"," +
+                               "\"shortName\": \"tqo\"" +
                            "}";
+
+            //request.ContentLength = requestBody.Length;
 
             using (Stream stream = this.GenerateStreamFromString(requestBody))
             {
@@ -102,6 +182,13 @@ namespace ARSnovaPPIntegration.Communication
             writer.Flush();
             stream.Position = 0;
             return stream;
+        }
+
+        private string ConvertToUnixTimestampString(DateTime dateTime)
+        {
+            var unixDateTimeStart = new DateTime(1970, 1, 1, 0, 0, 0, 0).ToLocalTime();
+            var dateTimeSpan = dateTime.ToUniversalTime() - unixDateTimeStart;
+            return Math.Floor(dateTimeSpan.TotalSeconds).ToString(CultureInfo.CurrentCulture);
         }
     }
 }
