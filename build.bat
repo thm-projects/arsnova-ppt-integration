2214569de95ccdb1d7e6ad5587e58169157c2575
Tjark Wilhelm Hoeck <tjark.wilhelm.hoeck@mni.thm.de>
2016-09-29 16:16:09 +0200

    add build file for ci

---
+++ build.bat
@@ -0,0 +1,21 @@
+@ECHO OFF
+
+SET msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+SET slnPath="C:\Projects\powerpoint-integration\ARSnovaPPIntegration\source\ARSnovaPPIntegration.sln"
+
+%msbuild% %slnPath%
+if errorlevel 1 goto BuildFail
+goto BuildSuccess
+
+:BuildFail
+echo.
+echo *** BUILD FAILED ***
+goto End
+
+:BuildSuccess
+echo.
+echo **** BUILD SUCCESSFUL ***
+goto end
+
+:End
+popd
