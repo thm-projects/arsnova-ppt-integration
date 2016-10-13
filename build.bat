9a45a65f5b3e9e0ad369c46c99069ac5f32d30b6
Tjark Wilhelm Hoeck <tjark.wilhelm.hoeck@mni.thm.de>
2016-10-13 15:13:56 +0200

    remove return value elavuation from build script for ci and force cleanup before build

--- build.bat
+++ build.bat
@@ -4,4 +4,4 @@ SET msbuildOld="%windir%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
 SET msbuild="C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe"
 SET slnPath="C:\Projects\powerpoint-integration\ARSnovaPPIntegration\source\ARSnovaPPIntegration.sln"
 
-%msbuild% %slnPath% /p:VisualStudioVersion=14.0 /p:ToolsVersion=14.0 /p:DefineConstants="CODE_ANALYSIS;VSTO40"
+%msbuild% %slnPath% /p:VisualStudioVersion=14.0 /p:ToolsVersion=14.0 /p:DefineConstants="CODE_ANALYSIS;VSTO40" /t:Clean,Build
